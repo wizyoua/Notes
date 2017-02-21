add837d612d84a033dcbdbb77d17b3834a4024f7
Gianni Zamora <giannizcool@gmail.com>
2017-02-21 11:11:20 -0700

    new notes

--- JavaScript Notes/Udemy/JS-The Weird Parts/app1.js
+++ JavaScript Notes/Udemy/JS-The Weird Parts/app1.js
@@ -8,3 +8,72 @@ var a = "hello world";
 function b(){
 	console.log("called b!");
 }
+
+
+//chapter 3 Lecture 25
+
+console.log(1 < 2 < 3); //true
+
+
+
+//chapter 4 objects
+
+//4,30 - not preffered way to create object
+var person = new Object();
+
+person["firstname"] = "Tony";
+person["lastname"] = "alice";
+
+var firstnameProperty = "firstname";
+
+console.log(person);
+console.log(person[firstnameProperty]);
+
+console.log(person.firstname);
+console.log(person.lastname);
+
+person.address = new Object();
+person.address.street = "11 Main St";
+person.address.city = "new york";
+
+console.log(person.address.street);
+
+
+
+//4.31 - object and object literals
+var person1 = {
+	firstname: 'tony',
+	lastname: 'alice',
+	address: {
+		street: '111 main st',
+		city: 'newyork',
+		state: 'ny'
+	}
+};
+
+
+function greet(person){
+	console.log("hi" + person.firstname);
+}
+
+greet(Tony);
+
+greet({
+	firstname: 'Mary', 
+	lastname: 'Doe'
+});
+
+Tony.address2 = {
+	street: '333 street'
+}
+
+console.log(person);
+
+//4.33 functions are objects
+
+function greet(){
+	console.log("hi");
+}
+greet.language = 'english';
+//property attached to the object
+console.log(greet.language);
