56aaeea148858c3c9312bd9a26e4cc5532e14ee7
Gianni Zamora <giannizcool@gmail.com>
2017-02-22 17:17:07 -0700

    updates

--- JavaScript Notes/Udemy/JS-The Weird Parts/app1.js
+++ JavaScript Notes/Udemy/JS-The Weird Parts/app1.js
@@ -69,7 +69,7 @@ Tony.address2 = {
 
 console.log(person);
 
-//4.33 functions are objects
+//4.34 functions are objects
 
 function greet(){
 	console.log("hi");
@@ -77,3 +77,175 @@ function greet(){
 greet.language = 'english';
 //property attached to the object
 console.log(greet.language);
+
+//4,35
+//can call greet because of hoisting remember 
+greet();
+
+function greet() {
+	console.log("Hi");
+}
+
+//fucntions are objects, we are setting it to that variable
+var anonGreet = function(){
+	console.log("hi");
+}
+
+//invoke the code
+anonGreet();
+
+
+function log(a){
+	//how to run thefunction that is passed to log
+	a();
+	console.log(a);
+}
+log(3);
+//passing function a function
+log(function(){
+	console.log("hi");
+});
+
+
+
+
+//4.36 
+// by value (primitives)
+var a = 3;
+var b;
+//this line copied a first before a changed to 2 from 4 (by value)
+b = a;
+a = 2; 
+
+
+//by reference (all objects including functions) - 
+var c = {greeting: 'hi'};
+var d;
+
+//d will point to same address as c
+d = c;
+c.greeting = 'hello'; // mutate
+
+console.log(c);
+console.log(d);
+
+
+//by reference (even as parameters)
+function changeGreeting(obj){
+	obj.greeting = 'hola';//muatte
+}
+changeGreeting(d);
+console.log(c);
+console.log(d);
+
+//equal operator sets up new memory space (new address)
+c = {greeting: 'howdy'};
+console.log(c);
+console.log(d);
+
+
+
+//4.37
+console.log(this);// show window/global object
+
+function a(){
+	console.log(this); // will point to gloabl object
+	this.newvariable = 'hello';
+}
+
+a();
+
+console.log(newvariable);// crashing into global namespace
+
+var b = function(){
+	console.log(this);
+}
+
+b();
+
+
+var c = {
+	name: 'C',
+	log: function(){
+		var self = this;
+
+		self.name = 'updated c object';
+		console.log(self);
+
+		var setname = function(newname){
+			self.name = newname;
+		}
+		setname('updates again! c object');
+		//will points to global object again
+		console.log(self)
+	}
+}
+
+c.log();
+
+
+//4.38
+//arrays can hold anything because of dynamic typing
+var arr = [
+	1,
+	false,
+	{
+		name: 'tony',
+		addy: '111 street'
+	},
+	function (name) {
+		 var greeting = 'Helo';
+	}
+];
+
+arr[3]arr([2].name);
+
+
+
+//4.39
+function greet(firstname, lastname, language) {
+
+	//set up default peramets if no argument is sent through
+	language = language || 'en';
+
+	if(arguments.l === 0) {
+		console.log('Missing params');
+		return;
+	}
+
+	console.log(firstname);
+	console.log(lastname);
+	console.log(language);
+	console.log(arguments);
+	console.log(arguments[0]);
+	console.log('-----');
+
+	//arguments will become depracated in the futue, the next thing is spread parameter uses ... to insert any extra arguments into auto array
+}
+
+greet();
+greet('john');
+greet('john','doe');
+greet('john','doe','english');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
