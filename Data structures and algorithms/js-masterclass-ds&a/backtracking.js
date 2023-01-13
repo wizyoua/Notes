09dea2d4bf805d221a4add0c4c3ce8dcf2ec68de
Gianni Zamora <giannizcool@gmail.com>
2023-01-13 01:00:17 -0800

    backtracking - trie - networking notes

---
+++ Data structures and algorithms/js-masterclass-ds&a/backtracking.js
@@ -0,0 +1,10 @@
+// backtracking is pretty much exploring all the possible solutions to a problem
+
+// Toolkit and patterns for backtracking
+//3 keys of backtracking
+//1. Choice: what choices do we have at each step?
+//2. Constraints: what constraints do we have?
+//3. Goal: what is the goal?
+
+//key words in backtracking problems: permutation, combination, subset, partition, compute
+//lets solve the soduku problem using backtracking
